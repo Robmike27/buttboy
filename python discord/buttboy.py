f8064e2ad6edb89b4fba6025758a7daa16f925d7
Robmike27 <38091371+Robmike27@users.noreply.github.com>
2020-12-30 14:57:45 -0800

    Add files via upload

---
+++ python discord/buttboy.py
@@ -0,0 +1,46 @@
+import discord
+
+client = discord.Client()
+frickSwears = 0
+hasCounted = False
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+    
+
+@client.event
+async def on_message(message):
+    global frickSwears
+    await channelCount(message.channel)
+
+    if message.author == client.user:
+        return
+
+    if ('hi' in message.content.lower() or 'hello' in message.content.lower()) and 'buttboy' in message.content.lower():
+        await message.channel.send("I'm Buttboy! Hello!")
+    
+    if 'fuck' in message.content.lower():
+        frickSwears += 1
+        await message.channel.send("Luggage :luggage: ")
+    
+    if message.content.startswith('point and laugh'):
+        await message.channel.send('hahaaahahahhhahaahaha')
+
+    if message.content.startswith('count'):
+        await message.channel.send('Ok...')
+        await message.channel.send('This channel has said the frick word '+ str(frickSwears) +' times!')
+
+#This function counts how many times a channel has the word fuck or any variaton
+async def channelCount(channel):
+    global hasCounted
+    global frickSwears
+    if hasCounted == False:
+        channel = await channel.history(limit=None).flatten()
+        for message in channel:
+            if 'fuck' in message.content.lower() or 'fucking' in message.content.lower():
+                frickSwears += 1
+            hasCounted = True
+    return frickSwears
+
+client.run('MzUwODU1NTgzMzAzNDY3MDA5.WaD1uQ.yek2iRTM6-al7hIG1mSurVqG9bY')
